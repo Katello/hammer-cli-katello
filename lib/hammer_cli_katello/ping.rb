92cc4c88a9d1f2aedf4560fc11feb55ced21b93a
Adam Price <komidore64@gmail.com>
2013-11-12 15:58:41 -0500

    switching to HammerCLI::Apipie::ReadCommand

--- lib/hammer_cli_katello/ping.rb
+++ lib/hammer_cli_katello/ping.rb
@@ -4,73 +4,61 @@ require 'hammer_cli_katello/resource'
 
 module KatelloCLIPing
 
-  class IndexCommand < HammerCLI::AbstractCommand
+  class IndexCommand < HammerCLI::Apipie::ReadCommand
     include HammerCLIKatello::Resource
 
-    def initialize(*args)
-      super(args)
-      @ping = KatelloApi::Resources::Ping.new(resource_config)
-    end
+    command_name "ping"
+    resource KatelloApi::Resources::Ping, :index
+    output do
+      from "status" do
 
-    def execute
-      #TODO there's gotta be a better way to do this output
-      dsl = HammerCLI::Output::Dsl.new
-      dsl.build do
-
-        from "status" do
-
-          label "candlepin" do
-            from "candlepin" do
-              field "result", "Result"
-              field "duration_ms", "Duration"
-            end
+        label "candlepin" do
+          from "candlepin" do
+            field "result", "Result"
+            field "duration_ms", "Duration"
           end
-
-          label "candlepin_auth" do
-            from "candlepin_auth" do
-              field "result", "Result"
-              field "duration_ms", "Duration"
-            end
-          end
-
-          label "pulp" do
-            from "pulp" do
-              field "result", "Result"
-              field "duration_ms", "Duration"
-            end
-          end
-
-          label "pulp_auth" do
-            from "pulp_auth" do
-              field "result", "Result"
-              field "duration_ms", "Duration"
-            end
-          end
-
-          label "elasticsearch" do
-            from "elasticsearch" do
-              field "result", "Result"
-              field "duration_ms", "Duration"
-            end
-          end
-
-          label "katello_jobs" do
-            from "katello_jobs" do
-              field "result", "Result"
-              field "duration_ms", "Duration"
-            end
-          end
-
         end
-      end
 
-      definition = HammerCLI::Output::Definition.new
-      definition.append(dsl.fields)
-      print_records(definition, @ping.index[0])
-      HammerCLI::EX_OK
-    end
+        label "candlepin_auth" do
+          from "candlepin_auth" do
+            field "result", "Result"
+            field "duration_ms", "Duration"
+          end
+        end
 
-  end
+        label "pulp" do
+          from "pulp" do
+            field "result", "Result"
+            field "duration_ms", "Duration"
+          end
+        end
+
+        label "pulp_auth" do
+          from "pulp_auth" do
+            field "result", "Result"
+            field "duration_ms", "Duration"
+          end
+        end
+
+        label "elasticsearch" do
+          from "elasticsearch" do
+            field "result", "Result"
+            field "duration_ms", "Duration"
+          end
+        end
+
+        label "katello_jobs" do
+          from "katello_jobs" do
+            field "result", "Result"
+            field "duration_ms", "Duration"
+          end
+        end
+
+      end # from "status"
+    end # output do
+
+  end # class IndexCommand
 
   HammerCLI::MainCommand.subcommand("ping", "ping the katello server", KatelloCLIPing::IndexCommand)
-end
+
+end # module KatelloCLIPing
