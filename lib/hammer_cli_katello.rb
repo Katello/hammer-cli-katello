0667faa2900520d68539360504b57bdbf9e3d91e
Michaux Kelley <michauxkelley@gmail.com>
2013-12-11 15:25:21 -0500

    Fix error because exception class does not exist

--- lib/hammer_cli_katello.rb
+++ lib/hammer_cli_katello.rb
@@ -4,12 +4,13 @@ require 'hammer_cli/exit_codes'
 module HammerCLIKatello
 
   def self.exception_handler_class
-    HammerCLIKatello::ExceptionHandler
+    HammerCLI::ExceptionHandler
   end
 
   # require 'hammer_cli_katello/stuff'
   # ...
   # ...
   require 'hammer_cli_katello/ping'
+  require 'hammer_cli_katello/product'
 
 end
