984dcdea9654d7baf15b5556d909e759a495cac8
Brad Buckingham <bbuckingham@redhat.com>
2014-02-11 09:59:42 -0500

    Content Views: initial support for puppet modules

---
+++ lib/hammer_cli_katello/puppet_module.rb
@@ -0,0 +1,46 @@
+module HammerCLIKatello
+
+  class PuppetModule < HammerCLI::Apipie::Command
+    resource KatelloApi::Resources::PuppetModule
+
+    class ListCommand < HammerCLIKatello::ListCommand
+      output do
+        field :id, "ID"
+        field :name, "Name"
+        field :version, "Version"
+        field :author, "Author"
+      end
+
+      apipie_options
+    end
+
+    class InfoCommand < HammerCLIKatello::InfoCommand
+      output do
+        field :id, "ID"
+        field :name, "Name"
+        field :version, "Version"
+        field :author, "Author"
+
+        field :summary, "Summary"
+        field :description, "Description"
+        field :license, "License"
+        field :project_page, "Project Page"
+        field :source, "Source"
+        field :dependencies, "Dependencies", Fields::List
+        field :checksums, "Checksums", Fields::List
+        field :tag_list, "Tag List", Fields::List
+      end
+
+      def request_params
+        super.merge(method_options)
+      end
+
+      apipie_options
+    end
+
+    autoload_subcommands
+  end
+end
+
+HammerCLI::MainCommand.subcommand "puppet-module", "View Puppet Module details.",
+                                  HammerCLIKatello::PuppetModule
