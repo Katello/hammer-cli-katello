af93c930f37a5f8a8125492ab21b64933cf0d4a1
David Davis <daviddavis@redhat.com>
2014-02-24 10:20:25 -0500

    Adding success and error messages for repository associations

--- lib/hammer_cli_katello/associating_commands.rb
+++ lib/hammer_cli_katello/associating_commands.rb
@@ -6,11 +6,17 @@ module HammerCLIKatello
       class AddRepositoryCommand < HammerCLIKatello::AddAssociatedCommand
         associated_resource KatelloApi::Resources::Repository
         apipie_options
+
+        success_message "The repository has been associated"
+        failure_message "Could not add repository"
       end
 
       class RemoveRepositoryCommand < HammerCLIKatello::RemoveAssociatedCommand
         associated_resource KatelloApi::Resources::Repository
         apipie_options
+
+        success_message "The repository has been removed"
+        failure_message "Could not remove repository"
       end
 
     end
