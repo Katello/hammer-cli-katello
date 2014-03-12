72ead9ec306832332bb302a04f125556bc10fe74
Tomas Strachota <tstrachota@redhat.com>
2014-03-12 14:15:16 +0000

    exception handling for not found

--- lib/hammer_cli_katello/exception_handler.rb
+++ lib/hammer_cli_katello/exception_handler.rb
@@ -20,10 +20,15 @@ module HammerCLIKatello
       HammerCLI::EX_DATAERR
     end
 
+    def handle_not_found(e)
+      handle_katello_error(e)
+      HammerCLI::EX_NOT_FOUND
+    end
+
     def handle_katello_error(e)
       response = JSON.parse(e.response)
       response = HammerCLIForeman.record_to_common_format(response)
-      print_error response["displayMessage"]
+      print_error response["displayMessage"] || response["full_messages"]
     end
 
   end
