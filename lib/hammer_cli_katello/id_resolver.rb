7488b3a8285eb2d252db35684400a2357f71308a
Tomas Strachota <tstrachota@redhat.com>
2014-06-11 16:21:56 +0000

    Fixes #6093 - better option descriptions
    - added custom descriptions for each searchable

--- lib/hammer_cli_katello/id_resolver.rb
+++ lib/hammer_cli_katello/id_resolver.rb
@@ -1,15 +1,29 @@
 module HammerCLIKatello
 
-  class Searchables
+  class Searchables < HammerCLIForeman::Searchables
 
     SEARCHABLES = {
-      :organization => [
-        HammerCLIForeman::Searchable.new("name", _("Name to search by")),
-        HammerCLIForeman::Searchable.new("label", _("Label to search by"), :editable => false)
-      ]
+      :activation_key =>        [s_name(_("Activation key name to search by"))],
+      :capsule =>               [s_name(_("Capsule name to search by"))],
+      :content_host =>          [s_name(_("Content host name to search by"))],
+      :content_view =>          [s_name(_("Content view name to search by"))],
+      :gpg =>                   [s_name(_("Gpg key name to search by"))],
+      :host_collection =>       [s_name(_("Host collection name to search by"))],
+      :lifecycle_environment => [s_name(_("Lifecycle environment name to search by"))],
+      :organization =>          [s_name(_("Organization name to search by")),
+                                 s("label", _("Organization label to search by"),
+                                   :editable => false)
+                                ],
+      :product =>               [s_name(_("Product name to search by"))],
+      :repository =>            [s_name(_("Repository name to search by"))],
+      :repository_set =>        [s_name(_("Repository set name to search by"))],
+      :subscription =>          [s_name(_("Subscription name to search by"))],
+      :sync_plan =>             [s_name(_("Sync plan name to search by"))],
+      :task =>                  [s_name(_("Task name to search by"))],
+      :user =>                  [s_name(_("User name to search by"))]
     }
 
-    DEFAULT_SEARCHABLES = [HammerCLIForeman::Searchable.new("name", _("Name to search by"))]
+    DEFAULT_SEARCHABLES = [s_name(_("Name to search by"))]
 
     def for(resource)
       SEARCHABLES[resource.singular_name.to_sym] || DEFAULT_SEARCHABLES
