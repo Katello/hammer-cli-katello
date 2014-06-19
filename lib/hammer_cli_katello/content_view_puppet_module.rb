0a0865f018662e443fd4bdc7a64d2c14dfa7c279
Tomas Strachota <tstrachota@redhat.com>
2014-06-19 13:18:57 +0000

    Refs #5958 - Remove commands content-view puppet-module update and info
    There's not much use for them. List command provides all available information already so there's no need to query for further details. Add and delete naturaly replaces update command as nothig can be really updated on the CV-module relation.

--- lib/hammer_cli_katello/content_view_puppet_module.rb
+++ lib/hammer_cli_katello/content_view_puppet_module.rb
@@ -27,39 +27,11 @@ module HammerCLIKatello
       build_options
     end
 
-    class InfoCommand < HammerCLIKatello::InfoCommand
-      output do
-        field :id, _("ID")
-        field :uuid, _("UUID"), Fields::Field, :hide_blank => true
-        field :name, _("Name")
-        field :author, _("Author")
-        field :created_at, _("Created"), Fields::Date
-        field :updated_at, _("Updated"), Fields::Date
-      end
-
-      def request_params
-        super.merge(method_options)
-      end
-
-      build_options
-    end
-
     class CreateCommand < HammerCLIKatello::CreateCommand
       command_name "add"
 
       success_message _("Puppet module added to content view")
       failure_message _("Could not add the puppet module")
-
-      build_options
-    end
-
-    class UpdateCommand < HammerCLIKatello::UpdateCommand
-      success_message _("Puppet module updated for content view")
-      failure_message _("Could not update the puppet module")
-
-      def request_params
-        super.merge(method_options)
-      end
 
       build_options
     end
