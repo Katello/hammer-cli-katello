bfb6fa89c4eecaefe7aeedecb5997b77691058c4
Partha Aji <paji@redhat.com>
2014-10-29 19:42:24 +0000

    Fixes #8203, #9226 - Docker counts displayed on repo info
    Docker Images / Docker Tag information can be now
    seen in the hammer repository info command

--- lib/hammer_cli_katello/docker_image.rb
+++ lib/hammer_cli_katello/docker_image.rb
@@ -9,10 +9,16 @@ module HammerCLIKatello
         field :id, _("ID")
         field :image_id, _("Image ID")
         field :size, _("Size")
+        field :tag_count, _("Tags")
       end
 
       build_options do |o|
         o.expand.including(:products, :organizations, :content_views)
+      end
+
+      def extend_data(data)
+        data["tag_count"] = data["tags"].size
+        data
       end
     end
 
