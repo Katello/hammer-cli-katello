119a101305c5e396b25dd2d374d86a979a09de27
Walden Raines <walden@waldenraines.com>
2016-02-02 09:27:16 -0500

    Fixes #13498: move content host errata to host.
    http://projects.theforeman.org/issues/13498

--- lib/hammer_cli_katello/host.rb
+++ lib/hammer_cli_katello/host.rb
@@ -1,8 +1,12 @@
 require 'hammer_cli_foreman/host'
+require 'hammer_cli_katello/host_errata'
 require 'hammer_cli_katello/host_package'
 require 'hammer_cli_katello/host_package_group'
 
 module HammerCLIKatello
+  HammerCLIForeman::Host.subcommand "errata",
+                                    HammerCLIKatello::HostErrata.desc,
+                                    HammerCLIKatello::HostErrata
 
   HammerCLIForeman::Host.subcommand "package",
                                     HammerCLIKatello::HostPackage.desc,
