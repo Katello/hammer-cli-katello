76dd9c5576363a90a79800e730676f9aac8c523f
Andrew Kofink <akofink@redhat.com>
2016-03-16 08:30:58 -0400

    Fixes #14147 - hammer content-view filter create ignores repos if name used instead of ID

---
+++ lib/hammer_cli_katello/search_options_creators.rb
@@ -0,0 +1,70 @@
+module HammerCLIKatello
+  module SearchOptionsCreators
+
+    def create_repositories_search_options(options)
+      name = options[HammerCLI.option_accessor_name("name")]
+      names = options[HammerCLI.option_accessor_name("names")]
+      product_id = options[HammerCLI.option_accessor_name("product_id")]
+
+      search_options = {}
+      search_options['name'] = name if name
+      search_options['names'] = names if names
+      search_options['product_id'] = product_id if product_id
+      search_options
+    end
+
+    def create_lifecycle_environments_search_options(options)
+      name = options[HammerCLI.option_accessor_name("name")]
+      organization_id = options[HammerCLI.option_accessor_name("organization_id")]
+
+      search_options = {}
+      search_options['name'] = name if name
+      if options['option_lifecycle_environment_names'] || name
+        search_options['organization_id'] = organization_id
+      end
+      search_options
+    end
+
+    def create_content_view_versions_search_options(options)
+      environment_id = options[HammerCLI.option_accessor_name("environment_id")]
+      content_view_id = options[HammerCLI.option_accessor_name("content_view_id")]
+      version = options[HammerCLI.option_accessor_name("version")]
+
+      search_options = {}
+
+      search_options['content_view_id'] = content_view_id if content_view_id
+      search_options['environment_id'] = environment_id if environment_id && content_view_id
+      search_options['version'] = version if version
+      search_options
+    end
+
+    def create_organizations_search_options(options)
+      create_search_options_without_katello_api(options, api.resource(:organizations))
+    end
+
+    def create_smart_proxies_search_options(options)
+      create_search_options_without_katello_api(options, api.resource(:smart_proxies))
+    end
+
+    def create_capsules_search_options(options)
+      create_search_options_without_katello_api(options, api.resource(:smart_proxies))
+    end
+
+    def create_hosts_search_options(options)
+      create_search_options_without_katello_api(options, api.resource(:hosts))
+    end
+
+    def create_search_options_with_katello_api(options, resource)
+      search_options = {}
+      searchables(resource).each do |s|
+        value = options[HammerCLI.option_accessor_name(s.name.to_s)]
+        if value
+          search_options.update("#{s.name}" => "#{value}")
+        end
+      end
+      search_options
+    end
+
+  end # module SearchOptionsCreators
+
+end # module HammerCLIKatello
---
+++ test/unit/search_options_creators_test.rb
@@ -0,0 +1,107 @@
+require_relative '../test_helper'
+require 'hammer_cli_katello/search_options_creators'
+
+class SearchOptionsCreatorsMock
+  include HammerCLIKatello::SearchOptionsCreators
+
+  def one
+  end
+
+  def two
+  end
+end
+
+describe HammerCLIKatello::SearchOptionsCreators do
+
+  let(:search_options_creators) { SearchOptionsCreatorsMock.new }
+  let(:options) { Hash.new }
+  let(:resource) { mock('ApipieBindings::Resource') }
+
+  describe '#create_repositories_search_options' do
+    it 'handles an array of names' do
+      search_options_creators.create_repositories_search_options(
+        'option_names' => ['repo1']
+      )['names'].must_equal ['repo1']
+    end
+
+    it 'handles a single value name' do
+      search_options_creators.create_repositories_search_options(
+        'option_name' => 'repo1'
+      )['name'].must_equal 'repo1'
+    end
+
+    it 'handles a single value product_id' do
+      search_options_creators.create_repositories_search_options(
+        'option_product_id' => 3
+      )['product_id'].must_equal 3
+    end
+  end
+
+  describe '#create_content_view_versions_search_options(options)' do
+    it 'handles a single value environment_id' do
+      search_options_creators.create_content_view_versions_search_options(
+        'option_environment_id' => 3,
+        'option_content_view_id' => :required_option
+      )['environment_id'].must_equal 3
+    end
+
+    it 'handles a single value content_view_id' do
+      search_options_creators.create_content_view_versions_search_options(
+        'option_content_view_id' => 3
+      )['content_view_id'].must_equal 3
+    end
+
+    it 'handles a single value version' do
+      search_options_creators.create_content_view_versions_search_options(
+        'option_version' => 3
+      )['version'].must_equal 3
+    end
+  end
+
+  describe 'without the katello api' do
+    let(:api) { mock('api') }
+
+    before(:each) do
+      search_options_creators.expects(:create_search_options_without_katello_api)
+      search_options_creators.stubs(:api).returns(api)
+      api.stubs(:resource)
+    end
+
+    describe '#create_organizations_search_options(options)' do
+      it 'does not use the katello api' do
+        search_options_creators.create_organizations_search_options(:anything)
+      end
+    end
+
+    describe '#create_smart_proxies_search_options(options)' do
+      it 'does not use the katello api' do
+        search_options_creators.create_organizations_search_options(:anything)
+      end
+    end
+
+    describe '#create_capsules_search_options(options)' do
+      it 'does not use the katello api' do
+        search_options_creators.create_organizations_search_options(:anything)
+      end
+    end
+
+    describe '#create_search_options_with_katello_api(options, resource)' do
+      it 'does not use the katello api' do
+        search_options_creators.create_organizations_search_options(:anything)
+      end
+    end
+
+  end # describe 'without the katello api'
+
+  describe '#create_search_options_with_katello_api' do
+    it 'translates all searchable fields from options' do
+      search_options_creators.stubs(:searchables).
+        returns([search_options_creators.method(:one), search_options_creators.method(:two)])
+
+      search_options_creators.create_search_options_with_katello_api(
+        {'option_one' => 1, 'option_two' => 2}, resource
+      ).must_equal('one' => '1', 'two' => '2')
+    end
+  end # describe '#create_search_options_with_katello_api'
+
+end # describe HammerCLIKatello::SearchOptionsCreators
