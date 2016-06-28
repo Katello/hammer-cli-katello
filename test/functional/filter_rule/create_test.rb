2a5a78aa30203d46d3e0a70234739e8fca52ea5e
Andrew Kofink <akofink@redhat.com>
2016-06-28 15:06:13 -0400

    Fixes #15537 - Allow package names as an array
    This change allows users to specify package or package group names in a
    comma-separated list when creating a content view filter rule rather
    than one package at a time.

---
+++ test/functional/filter_rule/create_test.rb
@@ -0,0 +1,20 @@
+require_relative '../test_helper'
+require 'hammer_cli_katello/filter_rule'
+
+module HammerCLIKatello
+  describe FilterRule::CreateCommand do
+    it 'allows minimal options' do
+      api_expects(:content_view_filter_rules, :create) do |p|
+        p['content_view_filter_id'] == 1 && p['name'] == 'rpm1'
+      end
+      run_cmd(%w(content-view filter rule create --content-view-filter-id 1 --name rpm1))
+    end
+
+    it 'allows multiple package names' do
+      api_expects(:content_view_filter_rules, :create) do |p|
+        p['content_view_filter_id'] == 1 && p['names'] == %w(rpm1 rpm2)
+      end
+      run_cmd(%w(content-view filter rule create --content-view-filter-id 1 --names rpm1,rpm2))
+    end
+  end
+end
