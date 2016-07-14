bfccf26399a5692485377d1103eddb74c26a14ba
Andrew Kofink <akofink@redhat.com>
2016-07-14 15:15:42 -0400

    Fixes #15693 - Error with proper message
    Error with the proper message when resolving content-view or
    lifecycle-environment ID with an organization-id

--- lib/hammer_cli_katello/hostgroup_extensions.rb
+++ lib/hammer_cli_katello/hostgroup_extensions.rb
@@ -8,6 +8,16 @@ module HammerCLIKatello
         _('Organization ID to search by')
       option '--lifecycle-environment-organization-id', 'ORGANIZATION_ID',
         _('Organization ID to search by')
+
+      validate_options do
+        if option(:option_lifecycle_environment_name).exist?
+          option(:option_lifecycle_environment_organization_id).required
+        end
+
+        if option(:option_content_view_name).exist?
+          option(:option_content_view_organization_id).required
+        end
+      end
     end
 
     ::HammerCLIForeman::Hostgroup::UpdateCommand.instance_eval do
@@ -16,6 +26,16 @@ module HammerCLIKatello
         _('Organization ID to search by')
       option '--lifecycle-environment-organization-id', 'ORGANIZATION_ID',
         _('Organization ID to search by')
+
+      validate_options do
+        if option(:option_lifecycle_environment_name).exist?
+          option(:option_lifecycle_environment_organization_id).required
+        end
+
+        if option(:option_content_view_name).exist?
+          option(:option_content_view_organization_id).required
+        end
+      end
     end
   end
 end
