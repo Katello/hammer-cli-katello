02e8526b5a8b3acb1e328839464c29ad4c38307f
Ranjan Kumar <ranjankumar188@gmail.com>
2016-08-05 19:34:47 +0530

    Fixes #16000 - Correct state of product content for an activation key

---
+++ test/functional/activaton_key/product_content_test.rb
@@ -0,0 +1,23 @@
+require File.join(File.dirname(__FILE__), '../test_helper')
+
+describe 'listing available product content' do
+  let(:activation_key_id) { 1 }
+  let(:empty_response_table) do
+    <<eostring
+---|------|------|-----|---------|-------|----------|---------
+ID | NAME | TYPE | URL | GPG KEY | LABEL | ENABLED? | OVERRIDE
+---|------|------|-----|---------|-------|----------|---------
+eostring
+  end
+
+  it "lists content available for an activation key" do
+    ex = api_expects(:activation_keys, :product_content) do |p|
+      p['id'] = activation_key_id
+    end
+    ex.returns(index_response([]))
+    assert_cmd(
+      success_result(empty_response_table),
+      run_cmd(%w(activation-key product-content --id 1))
+    )
+  end
+end
