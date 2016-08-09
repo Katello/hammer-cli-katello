d088e682ea7cd408a5c3b4a01074b9ece7552934
Andrew Kofink <akofink@redhat.com>
2016-08-09 15:30:52 -0400

    Fixes #16034 - Org resolution in AK add-host-coll
    Ensure organization ID resolution behaves properly in `hammer
    activation-key add/remove-host-collection`

--- lib/hammer_cli_katello/id_resolver.rb
+++ lib/hammer_cli_katello/id_resolver.rb
@@ -7,10 +7,7 @@ module HammerCLIKatello
       :capsule =>               [s_name(_("Capsule name to search by"))],
       :content_view =>          [s_name(_("Content view name to search by"))],
       :gpg =>                   [s_name(_("Gpg key name to search by"))],
-      :host_collection =>       [
-        s_name(_("Host collection name to search by")),
-        s("organization_id", _("Organization ID to search by"))
-      ],
+      :host_collection =>       [s_name(_("Host collection name to search by"))],
       :lifecycle_environment => [s_name(_("Lifecycle environment name to search by"))],
       :organization =>          [s_name(_("Organization name to search by")),
                                  s("label", _("Organization label to search by"),
