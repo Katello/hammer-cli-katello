4eb78e16797a7b05f5c035fd2f0eefaff43580d4
Tomas Strachota <tstrachota@redhat.com>
2017-02-17 12:26:02 +0000

    Fixes #18536 - using --name in host collection content actions

--- test/functional/host_collection/content_install_test.rb
+++ test/functional/host_collection/content_install_test.rb
@@ -1,37 +1,62 @@
 require_relative '../test_helper'
+require_relative './content_api_expectations'
 require 'hammer_cli_katello/host_collection'
 
 module HammerCLIKatello
   describe HostCollection::InstallContentBaseCommand do
-    it 'installs packages to hosts in a host collection' do
-      api_expects(:hosts_bulk_actions, :install_content) do |p|
-        p['organization_id'] == 1 &&
-          p['included'] == { search: "host_collection_id=\"3\"" } &&
-          p['content'] == ['wget']
-      end
+    include HammerCLIKatello::ContentAPIExpectations
 
+    def api_expects_content_install(content_type, expected_params)
+      api_expects_content_action(:install_content, content_type, expected_params)
+    end
+
+    it 'installs packages to hosts in a host collection' do
+      api_expects_content_install('package', :content => ['wget'])
       run_cmd(%w(host-collection package install --id 3 --packages wget --organization-id 1))
     end
 
-    it 'installs package groups to hosts in a host collection' do
-      api_expects(:hosts_bulk_actions, :install_content) do |p|
-        p['organization_id'] == 1 &&
-          p['included'] == { search: "host_collection_id=\"3\"" } &&
-          p['content'] == ['birds']
-      end
+    it 'installs packages to hosts in a host collection specified by name' do
+      api_expects_collection_search
+      api_expects_content_install('package', :content => ['wget'])
+      run_cmd(%w(host-collection package install --name Test --packages wget --organization-id 1))
+    end
 
+    it 'installs package groups to hosts in a host collection' do
+      api_expects_content_install('package_group', :content => ['birds'])
       run_cmd(%w(host-collection package-group install --id 3
                  --package-groups birds --organization-id 1))
     end
 
-    it 'installs erratum to hosts in a host collection' do
-      api_expects(:hosts_bulk_actions, :install_content) do |p|
-        p['organization_id'] == 1 &&
-          p['included'] == { search: "host_collection_id=\"3\"" } &&
-          p['content'] == ['Bird_Erratum']
-      end
+    it 'installs package groups to hosts in a host collection specified by name' do
+      api_expects_collection_search
+      api_expects_content_install('package_group', :content => ['birds'])
+      run_cmd(%w(host-collection package-group install --name Test
+                 --package-groups birds --organization-id 1))
+    end
 
+    it 'installs package groups to hosts in a host collection' do
+      api_expects_content_install('package_group', :content => ['birds'])
+      run_cmd(%w(host-collection package-group install --id 3
+                 --package-groups birds --organization-id 1))
+    end
+
+    it 'installs package groups to hosts in a host collection specified by name' do
+      api_expects_collection_search
+      api_expects_content_install('package_group', :content => ['birds'])
+      run_cmd(%w(host-collection package-group install --name Test
+                 --package-groups birds --organization-id 1))
+    end
+
+    it 'installs erratum to hosts in a host collection' do
+      api_expects_content_install('errata', :content => ['Bird_Erratum'])
       run_cmd(%w(host-collection erratum install --id 3 --errata Bird_Erratum --organization-id 1))
+    end
+
+    it 'installs erratum to hosts in a host collection specified by name' do
+      api_expects_collection_search
+      api_expects_content_install('errata', :content => ['Bird_Erratum'])
+      run_cmd(%w(host-collection erratum install --name Test --errata Bird_Erratum
+                 --organization-id 1))
     end
   end
 end
--- test/functional/host_collection/content_update_test.rb
+++ test/functional/host_collection/content_update_test.rb
@@ -1,26 +1,36 @@
 require_relative '../test_helper'
+require_relative './content_api_expectations'
 require 'hammer_cli_katello/host_collection'
 
 module HammerCLIKatello
   describe HostCollection::UpdateContentBaseCommand do
-    it 'updates packages on hosts in a host collection' do
-      api_expects(:hosts_bulk_actions, :update_content) do |p|
-        p['organization_id'] == 1 &&
-          p['included'] == { search: "host_collection_id=\"3\"" } &&
-          p['content'] == ['wget']
-      end
+    include HammerCLIKatello::ContentAPIExpectations
 
+    def api_expects_content_update(content_type, expected_params)
+      api_expects_content_action(:update_content, content_type, expected_params)
+    end
+
+    it 'updates packages on hosts in a host collection' do
+      api_expects_content_update('package', :content => ['wget'])
       run_cmd(%w(host-collection package update --id 3 --packages wget --organization-id 1))
     end
 
-    it 'updates package groups on hosts in a host collection' do
-      api_expects(:hosts_bulk_actions, :update_content) do |p|
-        p['organization_id'] == 1 &&
-          p['included'] == { search: "host_collection_id=\"3\"" } &&
-          p['content'] == ['birds']
-      end
+    it 'updates packages on hosts in a host collection specified by name' do
+      api_expects_collection_search
+      api_expects_content_update('package', :content => ['wget'])
+      run_cmd(%w(host-collection package update --name Test --packages wget --organization-id 1))
+    end
 
+    it 'updates package groups on hosts in a host collection' do
+      api_expects_content_update('package_group', :content => ['birds'])
       run_cmd(%w(host-collection package-group update --id 3
+                 --package-groups birds --organization-id 1))
+    end
+
+    it 'updates package groups on hosts in a host collection specified by name' do
+      api_expects_collection_search
+      api_expects_content_update('package_group', :content => ['birds'])
+      run_cmd(%w(host-collection package-group update --name Test
                  --package-groups birds --organization-id 1))
     end
   end
