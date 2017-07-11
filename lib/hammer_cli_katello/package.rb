48f69358458d8ffa35fee8d0e82de3884048993c
Andrew Kofink <ajkofink@gmail.com>
2017-07-11 16:19:02 -0400

    Fixes #20046 - Allow content view options (#501)

--- lib/hammer_cli_katello/package.rb
+++ lib/hammer_cli_katello/package.rb
@@ -13,18 +13,30 @@ module HammerCLIKatello
         organization_options = [:option_organization_id, :option_organization_name, \
                                 :option_organization_label]
         product_options = [:option_product_id, :option_product_name]
+        content_view_options = [:option_content_view_id, :option_content_view_name]
 
-        if option(:option_product_name).exist?
+        if option(:option_product_name).exist? || option(:option_content_view_name).exist?
           any(*organization_options).required
         end
 
         if option(:option_repository_name).exist?
           any(*product_options).required
         end
+
+        if option(:option_content_view_version_version).exist?
+          any(*content_view_options).required
+        end
+
+        if any(*content_view_options).exist?
+          any(:option_content_view_version_id,
+              :option_content_view_version_version,
+              :option_environment_id,
+              :option_environment_name).required
+        end
       end
 
       build_options do |o|
-        o.expand.including(:products)
+        o.expand.including(:products, :content_views)
       end
     end
 
