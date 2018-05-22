af920c6579950a9d67c7b669f8b3528b2bfffd5b
Tom McKay <thomasmckay@redhat.com>
2018-05-21 20:56:41 -0400

    refs #18964 - registry name pattern

--- test/functional/lifecycle_environment/update_test.rb
+++ test/functional/lifecycle_environment/update_test.rb
@@ -10,5 +10,14 @@ module HammerCLIKatello
 
       run_cmd(%w(lifecycle-environment update --id 3 --new-name le3))
     end
+
+    it 'allows registry-name-pattern option' do
+      api_expects(:lifecycle_environments, :update) do |p|
+        p['registry_name_pattern'] == '<%= repository.docker_upstream_name %>' && p['id'] == 3
+      end
+
+      run_cmd(%w(lifecycle-environment update --id 3
+                 --registry-name-pattern <%=\ repository.docker_upstream_name\ %>))
+    end
   end
 end
