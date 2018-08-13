23d795097a314b81c3614ee09eb7fb1bbb9acd08
cfouant <cfouant@redhat.com>
2018-08-13 08:31:26 -0400

    fixes #23993 - fixes entity filtering by adding searchables (#574)
    (cherry picked from commit 31e2fb825d4ec563cc069850a785f1e79f0a31cf)

--- lib/hammer_cli_katello/foreman_search_options_creators.rb
+++ lib/hammer_cli_katello/foreman_search_options_creators.rb
@@ -71,5 +71,13 @@ module HammerCLIKatello
     def create_packages_search_options(options, mode = nil)
       create_search_options_without_katello_api(options, api.resource(:packages), mode)
     end
+
+    def create_compute_resources_search_options(options, mode = nil)
+      create_search_options_without_katello_api(options, api.resource(:compute_resources), mode)
+    end
+
+    def create_image_search_options(options, mode = nil)
+      create_search_options_without_katello_api(options, api.resource(:images), mode)
+    end
   end
 end
