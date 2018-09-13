1d0d738213e7c560a86bbbb261803bc889c0fe16
Chris Roberts <chrobert@redhat.com>
2018-09-13 12:11:46 -0400

    Refs #24569 - Add 3.9 apidata & tests for cv publish version.

--- test/functional/content_view/publish_test.rb
+++ test/functional/content_view/publish_test.rb
@@ -2,6 +2,26 @@ require_relative '../test_helper'
 require 'hammer_cli_katello/content_view_puppet_module'
 
 module HammerCLIKatello
+  describe ContentView do
+    it 'allows major & minor' do
+      ex = api_expects(:organizations, :index) do |p|
+        p[:search] == "name = \"org1\""
+      end
+      ex.returns(index_response([{'id' => 1}]))
+
+      ex = api_expects(:content_views, :index) do |p|
+        p['name'] == 'cv' && p['organization_id'] == 1
+      end
+      ex.returns(index_response([{'id' => 3}]))
+
+      api_expects(:content_views, :publish) do |p|
+        p['id'] == 3 && p['major'] == 5 && p['minor'] == 1
+      end
+
+      run_cmd(%w(content-view publish --major 5 --minor 1 --organization org1 --name cv --async))
+    end
+  end
+
   describe ContentViewPuppetModule do
     it 'allows adding a puppet module' do
       ex = api_expects(:organizations, :index) do |p|
