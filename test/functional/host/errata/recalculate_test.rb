1136d549365f94952215fff07b997b4de0b42497
Jan Düpmeier <j.duepmeier@googlemail.com>
2018-11-07 15:59:37 +0100

    fixes #25404 - add command to recalculate errata for a host (#601)
    * fixes #25404 - add command to recalculate errata for a host
    
    * refs #25404 - Use with_params in tests for errata recalculation

---
+++ test/functional/host/errata/recalculate_test.rb
@@ -0,0 +1,42 @@
+require File.join(File.dirname(__FILE__), '../../test_helper')
+
+require 'hammer_cli_katello/content_view_puppet_module'
+
+describe 'recalculate errata' do
+  include ForemanTaskHelpers
+
+  before do
+    @cmd = %w(host errata recalculate)
+  end
+
+  let(:host_id) { 1 }
+  let(:task_id) { '5' }
+  let(:response) do
+    {
+      'id' => task_id,
+      'state' => 'stopped'
+    }
+  end
+
+  it "recalculates errata for a host" do
+    params = ["--host-id=#{host_id}"]
+
+    api_expects(:host_errata, :applicability, 'Host errata recalculate')
+      .with_params('host_id' => host_id)
+      .returns(response)
+
+    expect_foreman_task(task_id)
+
+    run_cmd(@cmd + params)
+  end
+
+  it "applies an errata to a host with async flag" do
+    params = ["--host-id=#{host_id}", "--async"]
+
+    api_expects(:host_errata, :applicability, 'Host errata recalculate')
+      .with_params('host_id' => host_id)
+      .returns(response)
+
+    run_cmd(@cmd + params)
+  end
+end
