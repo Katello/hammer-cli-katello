816b58ee34f9e3f66ae57dc0021755dd9452ee9a
Andrew Kofink <akofink@redhat.com>
2018-12-05 17:21:32 -0500

    Bump apipie cache to 3.11

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -17,7 +17,7 @@ require 'minitest/spec'
 require 'mocha/setup'
 require 'hammer_cli'
 
-KATELLO_VERSION = Gem::Version.new(ENV['TEST_API_VERSION'] || '3.10')
+KATELLO_VERSION = Gem::Version.new(ENV['TEST_API_VERSION'] || '3.11')
 
 if HammerCLI.context[:api_connection]
   HammerCLI.context[:api_connection].create('foreman') do
