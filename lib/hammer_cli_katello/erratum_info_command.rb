b0155c6fc42b037f898cf50d1558099772e1ba30
Partha Aji <paji@redhat.com>
2019-01-18 09:19:59 -0500

    Refs #25845 - Hide blank module streams

--- lib/hammer_cli_katello/erratum_info_command.rb
+++ lib/hammer_cli_katello/erratum_info_command.rb
@@ -14,9 +14,9 @@ module HammerCLIKatello
       field :issued, _("Issued")
       field :release, _("Release")
       field :solution, _("Solution")
-      field :packages, _("Packages"), Fields::List
+      field :packages, _("Packages"), Fields::List, :hide_blank => true
 
-      collection :module_streams, _("Module Streams") do
+      collection :module_streams, _("Module Streams"), :hide_blank => true do
         field :name, _("Name")
         field :stream, _("Stream")
         field :packages, _("Packages"), Fields::List
