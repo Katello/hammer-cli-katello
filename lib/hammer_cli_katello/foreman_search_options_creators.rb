1d3e98bf0c35b08c62489dcccfc7e6b316240280
Chris Roberts <chrobert@redhat.com>
2019-12-10 10:24:53 -0500

    Fixes #28178 - Correct http_proxy search by name

--- lib/hammer_cli_katello/foreman_search_options_creators.rb
+++ lib/hammer_cli_katello/foreman_search_options_creators.rb
@@ -87,5 +87,9 @@ module HammerCLIKatello
     def create_users_search_options(options, mode = nil)
       create_search_options_without_katello_api(options, api.resource(:users), mode)
     end
+
+    def create_http_proxies_search_options(options, mode = nil)
+      create_search_options_without_katello_api(options, api.resource(:http_proxies), mode)
+    end
   end
 end
