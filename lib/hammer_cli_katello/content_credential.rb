5c2a94c159df8d7130fc0ad73c8589085d9b441c
James Jeffers <jdjeffers@gmail.com>
2020-01-06 13:37:48 -0500

    Fixes #28293 - adds new command, deprectes gpg

---
+++ lib/hammer_cli_katello/content_credential.rb
@@ -0,0 +1,67 @@
+module HammerCLIKatello
+  class ContentCredentialCommand < HammerCLIForeman::Command
+    resource :content_credentials
+
+    class ListCommand < HammerCLIKatello::ListCommand
+      output do
+        field :id, _("ID")
+        field :name, _("Name")
+      end
+
+      build_options
+    end
+
+    class InfoCommand < HammerCLIKatello::InfoCommand
+      output do
+        field :id, _("ID")
+        field :name, _("Name")
+        from :organization do
+          field :name, _("Organization")
+        end
+
+        collection :repositories, "Repositories" do
+          field :id, _("ID")
+          field :name, _("Name")
+          field :content_type, _("Content Type")
+          from :product do
+            field :name, _("Product")
+          end
+        end
+
+        field :content, _("Content"), Fields::LongText
+      end
+
+      build_options
+    end
+
+    class CreateCommand < HammerCLIKatello::CreateCommand
+      success_message _("Content credential created.")
+      failure_message _("Could not create GPG key")
+
+      build_options :without => [:content]
+      option "--key", "GPG_KEY_FILE", _("GPG Key file"),
+             :attribute_name => :option_content,
+             :required => true,
+             :format => HammerCLI::Options::Normalizers::File.new
+    end
+
+    class UpdateCommand < HammerCLIKatello::UpdateCommand
+      success_message _("GPG Key updated.")
+      failure_message _("Could not update GPG Key")
+
+      build_options :without => [:content]
+      option "--key", "GPG_KEY_FILE", _("GPG Key file"),
+             :attribute_name => :option_content,
+             :format => HammerCLI::Options::Normalizers::File.new
+    end
+
+    class DeleteCommand < HammerCLIKatello::DeleteCommand
+      success_message _("GPG Key deleted.")
+      failure_message _("Could not delete the GPG Key")
+
+      build_options
+    end
+
+    autoload_subcommands
+  end
+end
