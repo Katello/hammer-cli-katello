b6f129a1b7245226499540e2aeb49a14a83cd1ed
John Mitsch <jomitsch@redhat.com>
2020-01-27 16:00:12 +0000

    Fixes #28294 - Create repo with ssl cert name
    This adds the ability to create a repository with a content credential names, not just ids
    
    For example:
    
    hammer repository create --product-id 3 --name my_repo --url https://example.com/myawesomerepo --content-type yum --ssl-client-key-name "my product key" --ssl-client-cert-name "my product cert" --ssl-ca-cert-name "my ca"

---
+++ test/functional/repository/create_test.rb
@@ -0,0 +1,51 @@
+require_relative '../test_helper'
+
+describe "create repository" do
+  let(:org_id) { 1 }
+  let(:product_id) { 2 }
+  let(:name) { "repo1" }
+  let(:content_type) { "yum" }
+
+  it 'with basic options' do
+    api_expects(:repositories, :create)
+      .with_params(
+        name: name,
+        product_id: product_id,
+        content_type: content_type)
+
+    command = %W(repository create --organization-id #{org_id} --product-id #{product_id}
+                 --content-type #{content_type} --name #{name})
+
+    assert_equal(0, run_cmd(command).exit_code)
+  end
+
+  it 'with ssl options by name' do
+    def stub_gpg_key(gpg_key_name)
+      gpg_key_index = api_expects(:gpg_keys, :index)
+                      .with_params(
+                        name: gpg_key_name.to_s,
+                        organization_id: 1,
+                        per_page: 1000,
+                        page: 1)
+      gpg_response = File.join(File.dirname(__FILE__), 'data', "#{gpg_key_name}.json")
+      gpg_key_index.returns(JSON.parse(File.read(gpg_response)))
+    end
+
+    api_expects(:repositories, :create)
+      .with_params(
+        name: name,
+        product_id: product_id,
+        ssl_ca_cert_id: 1,
+        ssl_client_cert_id: 2,
+        ssl_client_key_id: 3,
+        content_type: content_type)
+
+    %w(test_cert test_key test_ca).each { |cred| stub_gpg_key(cred) }
+
+    command = %W(repository create --organization-id #{org_id} --product-id #{product_id}
+                 --content-type #{content_type} --name #{name} --ssl-client-cert test_cert
+                 --ssl-client-key test_key --ssl-ca-cert test_ca)
+
+    assert_equal(0, run_cmd(command).exit_code)
+  end
+end
