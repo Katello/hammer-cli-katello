a1036e792c5a2e2e436d8f585753cc56d84b8a5a
Partha Aji <paji@redhat.com>
2020-11-23 02:10:09 -0500

    Refs #31076 - Remove commented out tests
    Uncommenting accidentally commented out tests in content-exports

--- test/functional/content_export/version_test.rb
+++ test/functional/content_export/version_test.rb
@@ -33,21 +33,21 @@ describe 'content-export version' do
   let(:version) { '10.0' }
   let(:destination_server) { "dream.example.com" }
 
-  # it "performs export with required options and async" do
-  #   params = [
-  #     '--id=2',
-  #     '--destination-server=foo',
-  #     '--async'
-  #   ]
+  it "performs export with required options and async" do
+    params = [
+      '--id=2',
+      '--destination-server=foo',
+      '--async'
+    ]
 
-  #   ex = api_expects(:content_exports, :version)
-  #   ex.returns(response)
+    ex = api_expects(:content_exports, :version)
+    ex.returns(response)
 
-  #   result = run_cmd(@cmd + params)
+    result = run_cmd(@cmd + params)
 
-  #   assert_equal("Content view version is being exported in task #{task_id}.\n", result.out)
-  #   assert_equal(HammerCLI::EX_OK, result.exit_code)
-  # end
+    assert_equal("Content view version is being exported in task #{task_id}.\n", result.out)
+    assert_equal(HammerCLI::EX_OK, result.exit_code)
+  end
 
   it "performs export with required options" do
     params = [
@@ -70,38 +70,38 @@ describe 'content-export version' do
     assert_equal(HammerCLI::EX_OK, result.exit_code)
   end
 
-  # it 'fails on missing required params' do
-  #   params = [
-  #     '--id=2'
-  #   ]
+  it 'fails on missing required params' do
+    params = [
+      '--id=2'
+    ]
 
-  #   result = run_cmd(@cmd + params)
-  #   expected_error = "Could not export the content view version:\n"
+    result = run_cmd(@cmd + params)
+    expected_error = "Could not export the content view version:\n"
 
-  #   assert_equal(result.exit_code, HammerCLI::EX_USAGE)
-  #   assert_equal(result.err[/#{expected_error}/], expected_error)
-  # end
+    assert_equal(result.exit_code, HammerCLI::EX_USAGE)
+    assert_equal(result.err[/#{expected_error}/], expected_error)
+  end
 
-  # it 'correctly resolves content-view-id and content view version number' do
-  #   params = ["--content-view-id=#{content_view_id}",
-  #             "--version=#{version}",
-  #             "--destination-server=#{destination_server}",
-  #             "--async"]
+  it 'correctly resolves content-view-id and content view version number' do
+    params = ["--content-view-id=#{content_view_id}",
+              "--version=#{version}",
+              "--destination-server=#{destination_server}",
+              "--async"]
 
-  #   cvv_expect = api_expects(:content_view_versions, :index) do |p|
-  #     assert_equal p['content_view_id'].to_s, content_view_id.to_s
-  #     assert_equal p["version"], version
-  #   end
+    cvv_expect = api_expects(:content_view_versions, :index) do |p|
+      assert_equal p['content_view_id'].to_s, content_view_id.to_s
+      assert_equal p["version"], version
+    end
 
-  #   cvv_expect.at_least_once.
-  #     returns(index_response([{'id' => content_view_version_id}]))
+    cvv_expect.at_least_once.
+      returns(index_response([{'id' => content_view_version_id}]))
 
-  #   ex = api_expects(:content_exports, :version) do |p|
-  #     assert_equal p['id'], content_view_version_id
-  #     assert_equal p["destination_server"], destination_server
-  #   end
-  #   ex.returns(response)
+    ex = api_expects(:content_exports, :version) do |p|
+      assert_equal p['id'], content_view_version_id
+      assert_equal p["destination_server"], destination_server
+    end
+    ex.returns(response)
 
-  #   run_cmd(@cmd + params)
-  # end
+    run_cmd(@cmd + params)
+  end
 end
