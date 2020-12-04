3879b5cd2f6894efea475d83bebc70ef899c18e8
Jonathon Turel <jturel@gmail.com>
2020-12-04 17:05:58 +0000

    Fixes #31438 - Add generate-metadata command for content-export

--- lib/hammer_cli_katello/content_export.rb
+++ lib/hammer_cli_katello/content_export.rb
@@ -6,6 +6,44 @@ module HammerCLIKatello
     desc "Prepare content for export to a disconnected Katello"
     resource :content_exports
 
+    class GenerateMetadataCommand < HammerCLIKatello::Command
+      desc _("Writes export metadata to disk for use by the importing Katello. This command "\
+        + "only needs to be used if the export was performed asynchronously "\
+        + "or if the metadata was lost")
+
+      command_name 'generate-metadata'
+
+      include ContentExportHelper
+
+      option "--task-id", "TASK_ID",
+             _("Generate metadata based on output of the specified export task"),
+             :attribute_name => :option_task_id,
+             :required => false
+
+      option "--id", "ID",
+             _("Generate metadata based on specified export history"),
+             :attribute_name => :option_export_id,
+             :required => false
+
+      def execute
+        export_history = if option_task_id
+                           export_task = reload_task(option_task_id)
+                           fetch_export_history_from_task(export_task)
+                         else
+                           fetch_export_history(option_export_id)
+                         end
+
+        unless export_history
+          raise _("No export history was found. Verify the value given for "\
+            + "--task-id or --id")
+        end
+
+        generate_metadata_json(export_history)
+
+        HammerCLI::EX_OK
+      end
+    end
+
     class ListCommand < HammerCLIKatello::ListCommand
       desc "View content view export histories"
       output do
