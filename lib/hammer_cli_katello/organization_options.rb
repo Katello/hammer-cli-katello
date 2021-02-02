0437b56efa815a7e4f2efe16331dbf90f08ac152
Oleh Fedorenko <ofedoren@redhat.com>
2021-02-02 18:36:51 +0000

    Refs #31741 - Squeeze options

--- lib/hammer_cli_katello/organization_options.rb
+++ lib/hammer_cli_katello/organization_options.rb
@@ -1,12 +1,14 @@
 module HammerCLIKatello
   module OrganizationOptions
     def self.included(base)
-      base.option '--organization-id', 'ORGANIZATION_ID',
-        _('Organization ID to search by'), attribute_name: :option_organization_id
-      base.option '--organization', 'ORGANIZATION_NAME',
-        _('Organization name to search by'), attribute_name: :option_organization_name
-      base.option '--organization-label', 'ORGANIZATION_LABEL',
-        _('Organization label to search by'), attribute_name: :option_organization_label
+      base.option_family do
+        parent '--organization-id', 'ORGANIZATION_ID',
+               _('Organization ID to search by'), attribute_name: :option_organization_id
+        child '--organization', 'ORGANIZATION_NAME',
+              _('Organization name to search by'), attribute_name: :option_organization_name
+        child '--organization-label', 'ORGANIZATION_LABEL',
+              _('Organization label to search by'), attribute_name: :option_organization_label
+      end
     end
 
     def org_options
