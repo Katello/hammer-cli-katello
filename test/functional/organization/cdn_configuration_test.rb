a6f67a99c271543821dbb167f86b1c5571e54906
Partha Aji <paji@redhat.com>
2021-10-29 16:38:21 -0400

    Refs #33783 - bindings for org cdn configurator

---
+++ test/functional/organization/cdn_configuration_test.rb
@@ -0,0 +1,42 @@
+require_relative '../test_helper'
+require 'hammer_cli_katello/organization'
+require 'hammer_cli_katello/associating_commands'
+
+describe HammerCLIKatello::Organization::ConfigureCdnCommand do
+  include OrganizationHelpers
+  before do
+    @cmd = %w(organization configure-cdn)
+  end
+
+  it 'by updates with the right params' do
+    org_id = '100'
+    org_label = 'org1'
+    ssl_cred_id = '1'
+    password = "pass"
+    username = "foo"
+    url = "https://goo.com"
+    upstream_label = "GreatOrg"
+    params = ["--label=#{org_label}",
+              "--url=#{url}",
+              "--username=#{username}",
+              "--upstream-organization-label=#{upstream_label}",
+              "--password=#{password}",
+              "--ssl-ca-credential-id=#{ssl_cred_id}"
+            ]
+    expect_organization_search(org_label, org_id, field: 'label')
+
+    api_expects(:organizations, :cdn_configuration) do |par|
+      par['id'].to_s == org_id &&
+        par['username'] == username &&
+        par['url'] == url &&
+        par['password'] == password &&
+        par['ssl_ca_credential_id'].to_s == ssl_cred_id &&
+        par['upstream_organization_label'] == upstream_label
+    end
+
+    assert_equal(
+      0,
+      run_cmd(@cmd + params).exit_code
+    )
+  end
+end
--- test/functional/organization/info_test.rb
+++ test/functional/organization/info_test.rb
@@ -19,4 +19,21 @@ describe HammerCLIKatello::Organization::InfoCommand do
     expected = success_result(FieldMatcher.new('Simple Content Access', 'Enabled'))
     assert_cmd(expected, result)
   end
+
+  it "includes cdn configuration attributes" do
+    org_id = 2
+    params = ["--id=#{org_id}"]
+    api_expects(:organizations, :index).returns(index_response([{'id' => org_id}]))
+
+    cdn_configuration = {
+      "url" => "https://cdn.redhat.com"
+    }
+
+    api_expects(:organizations, :show)
+      .with_params('id' => org_id.to_s)
+      .returns("cdn_configuration" => cdn_configuration, "id" => org_id)
+    result = run_cmd(@cmd + params)
+    expected = success_result(FieldMatcher.new('URL', cdn_configuration["url"]))
+    assert_cmd(expected, result)
+  end
 end
