1c0eeffa0b1b35d54d9b795f989514e5826a5e32
Partha Aji <paji@redhat.com>
2022-02-15 14:32:09 -0500

    Fixes #34454 - Hammer repo reclaim-space

--- test/functional/organization/cdn_configuration_test.rb
+++ test/functional/organization/cdn_configuration_test.rb
@@ -16,12 +16,14 @@ describe HammerCLIKatello::Organization::ConfigureCdnCommand do
     username = "foo"
     url = "https://goo.com"
     upstream_label = "GreatOrg"
+    type = 'upstream_server'
     params = ["--label=#{org_label}",
               "--url=#{url}",
               "--username=#{username}",
               "--upstream-organization-label=#{upstream_label}",
               "--password=#{password}",
-              "--ssl-ca-credential-id=#{ssl_cred_id}"
+              "--ssl-ca-credential-id=#{ssl_cred_id}",
+              "--type=#{type}"
             ]
     expect_organization_search(org_label, org_id, field: 'label')
 
@@ -31,7 +33,8 @@ describe HammerCLIKatello::Organization::ConfigureCdnCommand do
         par['url'] == url &&
         par['password'] == password &&
         par['ssl_ca_credential_id'].to_s == ssl_cred_id &&
-        par['upstream_organization_label'] == upstream_label
+        par['upstream_organization_label'] == upstream_label &&
+        par['type'] == type
     end
 
     assert_equal(
