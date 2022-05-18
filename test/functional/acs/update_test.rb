815d467ff9a6364d6356d84e6b186931aedefc7e
Chris Roberts <chrobert@redhat.com>
2022-05-18 11:13:22 -0400

    Fixes #34033 - Add CRUD Hammer ACS commands (#845)
    (cherry picked from commit 50150395eb606b210e572924230bea71bef338ac)

---
+++ test/functional/acs/update_test.rb
@@ -0,0 +1,23 @@
+require File.join(File.dirname(__FILE__), '../test_helper')
+
+describe 'listing acs' do
+  before do
+    @cmd = %w(alternate-content-sources update)
+  end
+
+  let(:id) { 1 }
+  let(:desc) { 'pizza' }
+
+  it 'update acs' do
+    params = ["--id=#{id}", "--description=#{desc}"]
+
+    ex = api_expects(:alternate_content_sources, :update, 'acs update') do |par|
+      par['id'] == 1 && par['description'] == 'pizza'
+    end
+
+    ex.returns({})
+
+    result = run_cmd(@cmd + params)
+    assert_equal(result.exit_code, 0)
+  end
+end
