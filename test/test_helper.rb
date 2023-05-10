628bac043b995321a17c7ffe08f8bccc54340c56
Chris Roberts <chrobert@redhat.com>
2023-05-10 15:33:27 -0400

    Fixes #36381 - Add Katello 4.9 test data and fix tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,23 +1,10 @@
-if RUBY_VERSION > "2.2"
-  # Coverage - Keep these two lines at the top of this file
-  require 'simplecov'
-  require 'coveralls'
-  SimpleCov.formatters = [SimpleCov::Formatter::HTMLFormatter, Coveralls::SimpleCov::Formatter]
-  SimpleCov.start do
-    minimum_coverage 70
-    maximum_coverage_drop 0.1
-    track_files "lib/**/*.rb"
-    add_filter '/test/'
-  end
-end
-
 require File.join(File.dirname(__FILE__), './task_helper.rb')
 require 'minitest/autorun'
 require 'minitest/spec'
 require 'mocha/minitest'
 require 'hammer_cli'
 
-KATELLO_VERSION = Gem::Version.new(ENV['TEST_API_VERSION'] || '4.7')
+KATELLO_VERSION = Gem::Version.new(ENV['TEST_API_VERSION'] || '4.9')
 
 if HammerCLI.context[:api_connection]
   HammerCLI.context[:api_connection].create('foreman') do
