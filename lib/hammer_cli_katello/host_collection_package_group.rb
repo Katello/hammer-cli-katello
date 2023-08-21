ff7d5d71b55920284974134c11d4611c40675bbc
Partha Aji <paji@redhat.com>
2023-08-21 16:12:39 -0400

    Refs #36665 - Remove katello-agent end points
    Update lib/hammer_cli_katello/host_package_group.rb
    
    Co-authored-by: Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
    
    Updated Tests
    
    Refs #36665 - Remove agent related actions from host collection

--- lib/hammer_cli_katello/host_collection_package_group.rb
+++ lib/hammer_cli_katello/host_collection_package_group.rb
@@ -1,72 +1,35 @@
 module HammerCLIKatello
   class HostCollectionPackageGroupCommand < HammerCLIKatello::Command
     command_name "package-group"
-    desc _("Manipulate package-groups for a host collection")
+    desc "Manage package-groups on your host collections. "\
+          "These commands are no longer available. "\
+          "Use the remote execution equivalent"
 
-    module PackageGroupContentType
-      def self.included(base)
-        base.option('--package-groups',
-          'PACKAGE-GROUPS',
-          _("comma-separated list of package-groups to install"),
-          :required => true,
-          :format => HammerCLI::Options::Normalizers::List.new,
-          :attribute_name => :content)
+    class InstallCommand < HammerCLIKatello::Command
+      command_name "install"
+      def self.rex_feature
+        "katello_group_install"
       end
 
-      def content_type
-        'package_group'
-      end
+      include UnsupportedKatelloAgentCommandHelper
     end
 
-    class InstallCommand < HammerCLIKatello::HostCollection::InstallContentBaseCommand
-      include PackageGroupContentType
-      desc _("Install package-groups on content hosts contained within a host collection")
-      success_message _("Successfully scheduled installation of package-group(s).")
-      failure_message _("Could not schedule installation of package-group(s)")
-
-      def execute
-        warn "This command uses katello agent and will be removed in favor of remote execution " \
-          "in Katello 4.10."
-        warn "The remote execution equivalent is `hammer job-invocation create --feature " \
-          "katello_group_install`. Specify the host collection with the --search-query " \
-          "parameter, e.g. `--search-query \"host_collection = MyCollection\"` or " \
-          "`--search-query \"host_collection_id=6\"`."
-        super
+    class UpdateCommand < HammerCLIKatello::Command
+      command_name "update"
+      def self.rex_feature
+        "katello_group_update"
       end
+
+      include UnsupportedKatelloAgentCommandHelper
     end
 
-    class UpdateCommand < HammerCLIKatello::HostCollection::UpdateContentBaseCommand
-      include PackageGroupContentType
-      desc _("Update package-groups on content hosts contained within a host collection")
-      success_message _("Successfully scheduled update of package-groups(s).")
-      failure_message _("Could not schedule update of package-group(s)")
-
-      def execute
-        warn "This command uses katello agent and will be removed in favor of remote execution " \
-          "in Katello 4.10."
-        warn "The remote execution equivalent is `hammer job-invocation create --feature " \
-          "katello_group_update`. Specify the host collection with the --search-query " \
-          "parameter, e.g. `--search-query \"host_collection = MyCollection\"` or " \
-          "`--search-query \"host_collection_id=6\"`."
-        super
+    class RemoveCommand < HammerCLIKatello::Command
+      command_name "remove"
+      def self.rex_feature
+        "katello_group_remove"
       end
-    end
 
-    class RemoveCommand < HammerCLIKatello::HostCollection::RemoveContentBaseCommand
-      include PackageGroupContentType
-      desc _("Remove package-groups on content hosts contained within a host collection")
-      success_message _("Successfully scheduled removal of package-groups(s).")
-      failure_message _("Could not schedule removal of package-group(s)")
-
-      def execute
-        warn "This command uses katello agent and will be removed in favor of remote execution " \
-          "in Katello 4.10."
-        warn "The remote execution equivalent is `hammer job-invocation create --feature " \
-          "katello_group_remove`. Specify the host collection with the --search-query " \
-          "parameter, e.g. `--search-query \"host_collection = MyCollection\"` or " \
-          "`--search-query \"host_collection_id=6\"`."
-        super
-      end
+      include UnsupportedKatelloAgentCommandHelper
     end
 
     autoload_subcommands
