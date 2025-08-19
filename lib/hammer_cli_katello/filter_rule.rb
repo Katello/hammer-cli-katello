e02abbef88763ea3d4cba5bdafda58f3a5f8d3ae
Samir Jha <samirjha1525@gmail.com>
2025-08-19 13:37:18 -0400

    Fixes #38678 - Add module stream Id to hammer filter rule (#1000)

--- lib/hammer_cli_katello/filter_rule.rb
+++ lib/hammer_cli_katello/filter_rule.rb
@@ -19,6 +19,7 @@ module HammerCLIKatello
         field :errata_id, _("Errata ID")
         field :start_date, _("Start Date")
         field :end_date, _("End Date")
+        field :module_stream_id, _("Module Stream ID")
       end
 
       build_options
@@ -41,6 +42,7 @@ module HammerCLIKatello
         field :end_date, _("End Date"), Fields::Field, :hide_blank => true
         field :date_type, _("Date Type"), Fields::Field, :hide_blank => true
         field :types, _("Types"), Fields::List, :hide_blank => true
+        field :module_stream_id, _("Module Stream ID"), Fields::Field, :hide_blank => true
         field :created_at, _("Created"), Fields::Date
         field :updated_at, _("Updated"), Fields::Date
       end
